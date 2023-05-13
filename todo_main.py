14a59a086b7e478d49a78c1e0d1e1deae939781a
Michael Faradei <faradei@mail.ru>
2023-05-13 19:39:54 +0300

    я сделал добавление задание на конкретный день при помощи календаря

---
+++ todo_main.py
@@ -0,0 +1,34 @@
+from config import TOKEN
+from telegram.ext import (
+    Updater,
+    Filters,
+    MessageHandler,
+    CommandHandler,
+    ConversationHandler
+)
+from start_menu import start, main_menu
+from interrupt import *
+from constants import *
+from task_create import add_handler
+
+updater = Updater(TOKEN) # Обновляет чат в поисках новых сообщений
+dispatcher = updater.dispatcher # распределительный центр
+
+conv_handler = ConversationHandler( #обработчик диалога
+    entry_points=[CommandHandler('start', start)], # точка входа в разговор
+    states={ # этапы разговора
+            MENU:[MessageHandler(Filters.text & ~Filters.command, main_menu)],
+            MENU_ITEMS:[
+                add_handler,
+                MessageHandler(Filters.text & ~Filters.command, wrong_message)
+            ]
+        },
+    fallbacks=[CommandHandler('end', end)] # точка выхода из разговора
+)
+
+
+dispatcher.add_handler(conv_handler)
+
+print('Сервер запущен!')
+updater.start_polling()
+updater.idle()  # ctrl + C
